1fae4e7b95b55965da69503ae81b64716268cacc
imkushalneupane <imkushalneupane@gmail.com>
2026-01-06 19:13:49 +0545

    Changes to weapon switch Ui wheel

--- Assets/Scripts/CrossHairVisiliblity.cs
+++ Assets/Scripts/CrossHairVisiliblity.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,14 +6,38 @@ public class CrossHairVisiliblity : MonoBehaviour
 {
     public Image _crossHair;
 
+    bool isWeaponSwtichUIActive = false;
+
+    private void Start()
+    {
+        SwitchWeapons.OnToggleWeaponSwitchUI += HandleWeaponSwitchUI;
+    }
+
+    private void HandleWeaponSwitchUI(bool isWSUIon)
+    {
+        isWeaponSwtichUIActive = isWSUIon;
+    }
+
     void Update()
     {
         //to show crosshair when right mouse button is held
         _crossHair.gameObject.SetActive(Input.GetMouseButton(1));
 
-        //to hide cursor 
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+
+
+        if (!isWeaponSwtichUIActive)
+        {
+            //to hide cursor 
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;  
+        }
+        else
+        {
+            //to show cursor
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
 
     }
 }
--- Assets/Scripts/SwitchWeapons.cs
+++ Assets/Scripts/SwitchWeapons.cs
@@ -1,66 +1,125 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class SwitchWeapons : MonoBehaviour
 {
-    public static event Action<int> OnWeaponSwitch;
+    public static event Action<int> OnWeaponSwitch;  // Event to switch weapons , to pass the weapon index
+    public static event Action<bool> OnToggleWeaponSwitchUI;  // Event to toggle weapon switch UI , using to pause the game
 
+    // References to weapon GameObjects
     [SerializeField] private GameObject pistol;
     [SerializeField] private GameObject shotgun;
     [SerializeField] private GameObject sniper;
 
-    [SerializeField] private GameObject switchWeaponUI;
+    // Reference to the weapon switch UI
+    [SerializeField] private GameObject switchWeaponWheel;
 
     private int currentWeaponIndex = 1;
+    private bool isWheelOpen = false;
+
+    private void Start()
+    {
+        if(switchWeaponWheel != null)
+        {
+            switchWeaponWheel.SetActive(false);
+        }
+    }
 
     void Update()
-    {   
+    {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-           
-            UnityEngine.Cursor.lockState = CursorLockMode.None;
-            UnityEngine.Cursor.visible = true;
-            Time.timeScale = 0f;
-            switchWeaponUI.SetActive(true);
+            OpenWheel();
         }
-        if (Input.GetKeyUp(KeyCode.Tab))
+
+        // Only close if wheel is open AND Tab is released
+        if (isWheelOpen && Input.GetKeyUp(KeyCode.Tab))
         {
-            Time.timeScale = 1f;
-            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-            UnityEngine.Cursor.visible = false;
-
-            switchWeaponUI.SetActive(false);
+            CloseWheel();
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Alpha1) && currentWeaponIndex != 1)
+    }
+    private void OpenWheel()
+    {
+        if (isWheelOpen) return;
+
+        isWheelOpen = true;
+        Time.timeScale = 0f;
+        switchWeaponWheel.SetActive(true);
+        OnToggleWeaponSwitchUI?.Invoke(isWheelOpen);
+    }
+
+    private void CloseWheel()
+    {
+        if (!isWheelOpen) return;
+
+        isWheelOpen = false;
+        Time.timeScale = 1f;
+        switchWeaponWheel.SetActive(false);
+        OnToggleWeaponSwitchUI?.Invoke(isWheelOpen);
+    }
+
+
+    private void DisableWeapons() //disabling all weapons before enabling the selected one
+    {
+        pistol.SetActive(false);
+        shotgun.SetActive(false);   
+        sniper.SetActive(false);
+    }
+
+    public void OnPistolSelected()
+    {
+        if (currentWeaponIndex != 1) 
         {
             OnWeaponSwitch?.Invoke(1);
             DisableWeapons();
             currentWeaponIndex = 1;
             pistol.SetActive(true);
+
+            CloseWheel();
+
+            
+            
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) && currentWeaponIndex != 2)
+
+    }
+    public void OnShotgunSelected()
+    {
+        if (currentWeaponIndex != 2) 
         {
             OnWeaponSwitch?.Invoke(2);
             DisableWeapons();
             currentWeaponIndex = 2;
             shotgun.SetActive(true);
+
+            CloseWheel();
+
+            
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3) && currentWeaponIndex != 3)
+    }
+    public void OnSniperSelected()
+    {
+        if (currentWeaponIndex != 3) 
         {
             OnWeaponSwitch?.Invoke(3);
             DisableWeapons();
             currentWeaponIndex = 3;
             sniper.SetActive(true);
+
+            CloseWheel();
+
+           
+            
         }
     }
-
-    private void DisableWeapons()
+    public void OnHealingSelected()
     {
-        pistol.SetActive(false);
-        shotgun.SetActive(false);   
-        sniper.SetActive(false);
+        CloseWheel();
+
+  
+        
     }
 }
