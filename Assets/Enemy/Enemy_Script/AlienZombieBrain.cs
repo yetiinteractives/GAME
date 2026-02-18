931442a96f577e797e898f04ba11afc2b31493fe
Jeevsan Shrestha <jeepitwitter@gmail.com>
2026-02-18 21:26:49 +0545

    new enemy(AlienZombie)

---
+++ Assets/Enemy/Enemy_Script/AlienZombieBrain.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AlienZombieBrain : UniversalEnemyAi
+{
+    public enum AlienZombieState { Idle, Chase, Attack}
+    public AlienZombieState state;
+
+  
+    bool attackInProgress = false;
+     
+
+    [Header("Movement")]
+    public float WalkSpeed = 2f;
+
+    [Header("Detection & Combat")]
+    public float lookRadius = 30f;
+    public float attackRange = 2f;
+    public int attackDamage = 10;
+    public float attackCooldown = 2;
+    float attackTimer = 0f;
+
+    protected override void OnEnemyAwake()
+    {
+        state = AlienZombieState.Idle;
+       
+
+    }
+
+    protected override void HandleAI()
+    {
+        if (player == null) return;
+        float dist = distanceToPlayer;
+        if (dist <= attackRange)
+            state = AlienZombieState.Attack;
+        else if (dist <= lookRadius)
+        {    
+          state = AlienZombieState.Chase;
+            
+   
+
+        }
+        else
+            state = AlienZombieState.Idle;
+
+        if(state == AlienZombieState.Chase || state == AlienZombieState.Attack)
+            FacePlayer();
+
+
+        switch (state)
+        {
+            case AlienZombieState.Idle:
+               StopAgent();
+                PlayIdleAnimation();
+                attackInProgress = false;
+                break;
+
+            case AlienZombieState.Chase:
+
+                PlayWalkAnimation();
+                MoveAgent(WalkSpeed);
+                attackInProgress = false;
+                agent.SetDestination(player.position);
+
+               
+                break;
+
+            case AlienZombieState.Attack:
+                StopAgent();
+                if (!attackInProgress)
+                {
+                    anim.SetBool("Walk", false);
+                    attackInProgress = true;
+                    PlayAttackAnimation();
+                    attackTimer = 0f;
+                }
+
+                attackTimer += Time.deltaTime;
+                if (attackTimer >= attackCooldown)
+                {
+                    attackInProgress = false;
+                    //appy damage to player yeta
+                }
+                break;
+ 
+        }
+
+    }
+
+    private void FacePlayer()
+    {
+        if (player == null) return;
+
+        Vector3 direction = (player.position - transform.position).normalized;
+        direction.y = 0; // keep only horizontal rotation
+        if (direction == Vector3.zero) return;
+
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 5f * Time.deltaTime);
+    }
+    private void StopAgent()
+    {
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+    }
+    private void MoveAgent(float speed)
+    {
+        agent.isStopped = false;
+        agent.speed = speed;
+
+    }
+
+
+    protected override void OnDamageTaken(int damage)
+    {
+
+
+
+    }
+
+    protected override void OnEnemyDeath()
+    {
+        //for loot and some items
+    }
+
+}
