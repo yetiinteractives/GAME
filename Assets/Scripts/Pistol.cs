53b1fcc33f97e4661b3b7aa7aa93a90f51f1c0bb
imkushalneupane <imkushalneupane@gmail.com>
2026-03-02 02:31:21 +0545

    silencer UI changes

--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -1,43 +1,84 @@
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 using UnityEngine.UI;
 
 public class Pistol : Weapon
 {
+    [Header("Silencer")]
+    [SerializeField] private int silencerDurability = 5;
+
     [SerializeField] private GameObject silencerModel;
     [SerializeField] private AudioClip silencerSound;
-    [SerializeField] private Image silencerIcon;
 
-    [SerializeField] private int silencerDurability = 5;
-    
+    [Header("UI")]
+    [SerializeField] private Image silencerIcon;
+    private Vector2 originalIconPosition;
+    private Vector2 detachedOffset = new Vector2(10, 0);
+
+    [SerializeField] private Color silencerAttachedColor = Color.white;
+    [SerializeField] private Color silencerDetachedColor = Color.gray;
+
+    [Header("Sound")]
+    [SerializeField, Range(0.01f, 1f)]
+    private float silencedLoudnessMultiplier = 0.1f;
+
+    private RectTransform iconRect;
+
+    private bool hasSilencer = true;
+    private bool isSilencerOn;
+
     private int currentSilencerDurability;
 
-    [Header("Silencer")]
-    [SerializeField, Range(0.01f, 1f)] private float silencedLoudnessMultiplier = 0.1f;
-
-    private AudioClip currentShootSound;
-    private bool isSilencerOn;
     private float baseGunshotLoudness;
+    private AudioClip currentShootSound;
 
     protected override void Awake()
     {
         base.Awake();
 
+        iconRect = silencerIcon.rectTransform;
+
         baseGunshotLoudness = gunshotLoudness;
-        RemoveSilencer();
         currentSilencerDurability = silencerDurability;
+
+        UpdateUI(false);
+        RemoveSilencer();
+    }
+    private void Start()
+    {
+        originalIconPosition = silencerIcon.rectTransform.localPosition;
+    }
+
+    void UpdateUI(bool attached)
+    {
+        if (silencerIcon == null) return;
+
+        silencerIcon.gameObject.SetActive(hasSilencer);
+
+        if (attached)
+        {
+            silencerIcon.color = silencerAttachedColor;
+            //iconRect.localPosition = originalIconPosition;
+        }
+        else
+        {
+            silencerIcon.color = silencerDetachedColor;
+            //iconRect.localPosition = originalIconPosition + detachedOffset;
+        }
     }
 
     public void SetSilencer()
     {
+        if (!hasSilencer) return;
+
         isSilencerOn = true;
 
         if (silencerModel != null)
             silencerModel.SetActive(true);
 
-        silencerIcon.gameObject.SetActive(true);
         currentShootSound = silencerSound;
         gunshotLoudness = baseGunshotLoudness * silencedLoudnessMultiplier;
+
+        UpdateUI(true);
     }
 
     public void RemoveSilencer()
@@ -47,9 +88,10 @@ public class Pistol : Weapon
         if (silencerModel != null)
             silencerModel.SetActive(false);
 
-        silencerIcon.gameObject.SetActive(false);
         currentShootSound = shootSound;
         gunshotLoudness = baseGunshotLoudness;
+
+        UpdateUI(false);
     }
 
     protected override void Shoot(RaycastHit hit)
@@ -57,41 +99,43 @@ public class Pistol : Weapon
         ApplyDamage(hit, damage);
 
         bulletOnMag--;
-        if (isSilencerOn)
-        {
-            currentSilencerDurability--;
-        }
+
+       
+
         nextFireTime = Time.time + fireRate;
 
         NotifyShotEvent();
 
-        if (audioSource != null && currentShootSound != null)
+        if (audioSource && currentShootSound)
             audioSource.PlayOneShot(currentShootSound);
 
-        // Notify AI sound system
         SoundEmitter.EmitSoundAt(transform.position, SoundType.Gunshot, gunshotLoudness, gameObject);
 
-        if (muzzleFlash != null)
+        if (muzzleFlash)
             StartCoroutine(PlayMuzzleFlash());
 
-        if (ImpactManager.Instance != null)
+        if (ImpactManager.Instance)
             ImpactManager.Instance.SpawnImpact(gunType, hit);
 
         CinemachineShake.Instance.Shake(recoilIntensity, recoilDuration);
 
         OnShoot(hit);
 
-        if (bulletOnMag <= 0)
-            UpdateStatus("Out of Ammo!");
-        else
-            UpdateStatus("Aiming");
+        UpdateStatus(bulletOnMag <= 0 ? "Out of Ammo!" : "Aiming");
 
         Debug.Log($"{gameObject.name} fired! Mag: {bulletOnMag}/{magCapacity}, Total: {totalBullet}, Loudness: {gunshotLoudness:F2}");
-    
-        if(currentSilencerDurability <= 0)
+
+        if (isSilencerOn)
         {
-            RemoveSilencer();
+            currentSilencerDurability--;
+
+            if (currentSilencerDurability <= 0)
+            {
+                hasSilencer = false;
+                RemoveSilencer();
+            }
         }
+
     }
 
     protected override void Update()
@@ -100,6 +144,8 @@ public class Pistol : Weapon
 
         if (Input.GetKeyDown(KeyCode.E))
         {
+            if (!hasSilencer) return;
+
             if (isSilencerOn)
                 RemoveSilencer();
             else
