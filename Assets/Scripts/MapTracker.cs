cf07b4de241420b838d4fb9ec39a880c53a3a3fc
imkushalneupane <imkushalneupane@gmail.com>
2026-05-13 01:13:32 +0545

    map Toggle with M

--- Assets/Scripts/MapTracker.cs
+++ Assets/Scripts/MapTracker.cs
@@ -1,21 +1,58 @@
 using UnityEngine;
 
-public class MinimapTracker : MonoBehaviour
+public class MapTracker : MonoBehaviour
 {
     [Header("References")]
     public Transform player;
     public Camera captureCamera;
 
-    [Header("UI")]
+    [Header("UI Root")]
+    public GameObject mapFrame;
+
+    [Header("UI Elements")]
     public RectTransform mapRect;
     public RectTransform markerRect;
 
-    [Header("Rotation Fix ")]
-    public float rotationOffset = 90f;
+    [Header("Rotation Fix")]
+    public float rotationOffset = -90f;
+
+    private bool isMapOpen = false;
 
     void Update()
     {
-        
+        HandleToggle();
+
+        if (!isMapOpen) return;
+
+        UpdateMinimap();
+    }
+
+    void HandleToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isMapOpen = !isMapOpen;
+            mapFrame.SetActive(isMapOpen);
+
+            if (isMapOpen)
+                OnMapOpened();
+            else
+                OnMapClosed();
+        }
+    }
+
+    void OnMapOpened()
+    {
+        UpdateMinimap(); 
+    }
+
+    void OnMapClosed()
+    {
+    }
+
+    
+    void UpdateMinimap()
+    {
         Vector3 vp = captureCamera.WorldToViewportPoint(player.position);
 
         float x = (vp.x - 0.5f) * mapRect.rect.width;
