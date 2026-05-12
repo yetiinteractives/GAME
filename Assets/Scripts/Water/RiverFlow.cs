ba2f8183e905098e6b9553eca7c22cd23215d9b1
Jeevsan Shrestha <jeepitwitter@gmail.com>
2026-05-12 08:10:48 +0545

    riverflow logic written

---
+++ Assets/Scripts/Water/RiverFlow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RiverFlow : MonoBehaviour
+{
+    public float speed = 0.1f;
+    Renderer rend;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+        rend.material.mainTextureOffset = new Vector2(0, offset);
+        
+    }
+}
