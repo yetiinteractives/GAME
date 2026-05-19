b7bde71a671580d1ede6b65cc281446a6f5c5951
Jeevsan Shrestha <jeepitwitter@gmail.com>
2026-05-19 18:55:07 +0545

    screen fade script

---
+++ Assets/Scripts/UI/ScreenFade.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ScreenFade : MonoBehaviour
+{
+    public Image blackimage;
+    public float fadeSpeed = 1f;
+
+    public void StartFade()
+    {
+        StartCoroutine(FadeToBlack());
+    }
+    IEnumerator FadeToBlack()
+    {
+        Color color = blackimage.color;
+        while (color.a < 1)
+        {
+            color.a += Time.deltaTime * fadeSpeed;
+            blackimage.color = color;
+            yield return null;
+        }
+    }
+}
