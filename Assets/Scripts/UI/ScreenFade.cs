98c1a811831c06a54e5d91a57a513818bd34ea79
Jeevsan Shrestha <jeepitwitter@gmail.com>
2026-05-19 19:10:41 +0545

    Fade out feature added to fade script

--- Assets/Scripts/UI/ScreenFade.cs
+++ Assets/Scripts/UI/ScreenFade.cs
@@ -21,8 +21,18 @@ public class ScreenFade : MonoBehaviour
             blackimage.color = color;
             yield return null;
         }
+
+        yield return new WaitForSeconds(0.2f);
+        //fade out 
+        while (color.a > 0)
+        {
+            color.a -= Time.deltaTime * fadeSpeed;
+            blackimage.color = color;
+            yield return null;
+
+        }
     }
-    private void Start()
+    public void Start()
     {
         StartFade();
     }
